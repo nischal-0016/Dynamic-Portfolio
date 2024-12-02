33b59d9a1f9a410d8753d9871906a5be1c7e1542
nischal-0016 <koiralanischal16@gmail.com>
2024-12-02 14:41:48 +0545

    first commit

---
+++ dynamicweb/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
