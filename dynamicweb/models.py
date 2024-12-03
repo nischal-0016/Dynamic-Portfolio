631ec19c3359d5cb9dc6823d9e1fe90a47a6ad29
nischal-0016 <koiralanischal16@gmail.com>
2024-12-03 11:06:03 +0545

    about section

--- dynamicweb/models.py
+++ dynamicweb/models.py
@@ -8,3 +8,20 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class AboutDetail(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    icon = models.ImageField(upload_to='about_icons/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class AboutMe(models.Model):
+    introduction = models.TextField()
+    arrow_image = models.ImageField(upload_to='about_icons/', null=True, blank=True)
+
+    def __str__(self):
+        return "About Me Section"
--- dynamicweb/views.py
+++ dynamicweb/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Project
+from .models import AboutDetail, AboutMe
+
 
 def portfolio(request):
     projects = Project.objects.all()
-    return render(request, 'pages/portfolio.html', {'projects': projects})
+    about_details = AboutDetail.objects.all()
+    about_me = AboutMe.objects.first()  # Assumes one "About Me" entry
+    return render(request, 'pages/portfolio.html', {
+        'projects': projects,
+        'about_details': about_details,
+        'about_me': about_me
+    })
