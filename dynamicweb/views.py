3b0a717903edbc671f18ad09101c6184853378fc
nischal-0016 <koiralanischal16@gmail.com>
2024-12-04 18:34:27 +0545

    profile section

--- dynamicweb/views.py
+++ dynamicweb/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from .models import Profile, AboutDetail, AboutMe, Skill
 
 def portfolio(request):
-    profiles = Profile.objects.first()  # Fetch the first Profile record
-    about_details = AboutDetail.objects.all()  # Fetch About details
-    about_me = AboutMe.objects.first()  # Get the first AboutMe record
-    frontend_skills = Skill.objects.filter(category='frontend')  # Frontend skills
-    backend_skills = Skill.objects.filter(category='backend')  # Backend skills
+    profiles = Profile.objects.first()  
+    about_details = AboutDetail.objects.all()  
+    about_me = AboutMe.objects.first()  
+    frontend_skills = Skill.objects.filter(category='frontend')
+    backend_skills = Skill.objects.filter(category='backend')  
 
     context = {
-        'profiles': profiles,  # Sending profiles to the template
+        'profiles': profiles,
         'about_details': about_details,
         'about_me': about_me,
         'frontend_skills': frontend_skills,
